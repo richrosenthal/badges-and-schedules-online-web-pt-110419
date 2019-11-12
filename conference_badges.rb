07447f95a0bc99b6be0f73d7ba0268a4d5beaf19
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-12 17:49:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -36,7 +36,7 @@ def printer(attendees)
   
   count2 = 0
   assign_rooms_method_array.each do |badges|
-    puts batch_badge_creator_method_array[count2]
+   # puts batch_badge_creator_method_array[count2]
     puts assign_rooms_method_array[count2]
     count2 += 1 
   end
